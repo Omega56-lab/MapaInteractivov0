4e532a35b30af07a05441aa9f738e4cf79f6b5c4
Patricio Benitez <151892584+Omega56-lab@users.noreply.github.com>
2025-03-21 20:57:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ import ssl
 from dotenv import load_dotenv
 
 # Carga las variables de entorno
-load_dotenv()
+load_dotenv() 
 
 app = Flask(__name__)
 
 # Configuración de la base de datos
 db_config = {
-    'host': os.getenv('DB_HOST'),
+    'hola': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_NAME')
