8341939c1dd63c39e9d7d1ce2fac94aa7fa7b629
Patricio Benitez <151892584+Omega56-lab@users.noreply.github.com>
2025-03-21 21:14:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 # Configuración de la base de datos
 db_config = {
-    'hola': os.getenv('DB_HOST'),
+    'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_NAME')
